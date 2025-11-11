7f31a545db7e9cae70f12f0c96c83334fb7ec63d
ajoyfuljester <ajoyfuljester@protonmail.com>
2025-11-11 22:48:21 +0100

    feat: add global configuration

--- config/global.go
+++ config/global.go
@@ -1,4 +1,21 @@
 package config
 
+import (
+	"os"
+)
 
 var Version = "0.0"
+
+
+var MusicDirectory string
+
+func init() {
+	dir, found := os.LookupEnv("MUSIC_DIR")
+	if !found {
+		dir = os.Getenv("HOME") + "/Music"
+	}
+
+	dir = dir + "/tuitune"
+
+	MusicDirectory = dir
+}
